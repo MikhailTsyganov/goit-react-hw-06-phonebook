47fae6766d28c525c0db032d3ab995c4de708a5b
MikhailTsyganov <tsyganov.mik@gmail.com>
2022-10-14 16:16:47 +0300

    Почистил App

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,3 @@
-import { useEffect, useRef } from 'react';
-
 import Filter from './Filter/Filter';
 import ContactForm from './ContactForm/ContactForm';
 import ContactList from './ContactList/ContactList';
@@ -7,16 +5,6 @@ import { connect } from 'react-redux';
 import s from './App.module.css';
 
 function App({ contacts }) {
-  const isFirstRender = useRef(true);
-  useEffect(() => {
-    if (isFirstRender.current) {
-      isFirstRender.current = false;
-      return;
-    }
-
-    return localStorage.setItem('contacts', JSON.stringify(contacts));
-  }, [contacts]);
-
   return (
     <div className={s.container}>
       <h2>Phonebook</h2>
